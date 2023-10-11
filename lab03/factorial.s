e621a2c6d209ef0c2530dba4bc9de238a42e6e59
wuyuesong <1377030423@qq.com>
2023-10-11 13:42:18 +0800

    lab03 finished

--- lab03/factorial.s
+++ lab03/factorial.s
@@ -22,3 +22,29 @@ main:
 
 factorial:
     # YOUR CODE HERE
+    # VERSION 1
+#     add t1, a0, x0
+#     addi t1, t1, -1
+# Loop:
+#     beq t1, x0, Exit_factorial
+#     mul a0, a0, t1
+#     addi t1, t1, -1
+#     jal x0, Loop
+
+# Exit_factorial:
+#     jr ra
+
+    # VERSION 2
+    addi t1, x0, 1
+    beq t1, a0, Exit_factorial
+    addi sp, sp, -8
+    sw ra 4(sp)
+    sw a0 0(sp)
+    addi a0, a0, -1
+    jal ra factorial
+    lw t0 0(sp)
+    lw ra 4(sp)
+    mul a0, t0, a0
+    addi sp, sp, 8
+Exit_factorial:
+    jr ra
