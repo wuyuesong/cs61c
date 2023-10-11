c177fcac88c2b1ba72afb74953241ac61d964e93
wuyuesong <1377030423@qq.com>
2023-10-11 20:44:07 +0800

    lab04 finished

--- lab04/discrete_fn.s
+++ lab04/discrete_fn.s
@@ -75,10 +75,21 @@ main:
 # a0 is the value we want to evaluate f at
 # a1 is the address of the "output" array (defined above).
 # Think: why might having a1 be useful?
-f:
-    # YOUR CODE GOES HERE!
+# f:
+#     # YOUR CODE GOES HERE!
+#     addi a0, a0, 3
+#     addi t1, x0, 4
+#     mul a0, a0, t1
+#     add a0, a1, a0
+#     lw a0, 0(a0)
+#     jr ra               # Always remember to jr ra after your function!
 
-    jr ra               # Always remember to jr ra after your function!
+f:
+    addi a0, a0, 3
+    slli a0, a0, 2
+    add a0, a1, a0
+    lw a0, 0(a0)
+    jr ra 
 
 print_int:
     mv a1, a0
