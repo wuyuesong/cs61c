c764951e869ce85d3af02282fb2b4ab691514bcc
root <root@DESKTOP-L4PLVQ4.localdomain>
2023-10-20 15:59:06 +0800

    lab09 finished and pull project

---
+++ lab/lab09/common.c
@@ -0,0 +1,107 @@
+#include <time.h>
+#include <stdio.h>
+#include <x86intrin.h>
+#include "common.h"
+
+long long int sum(unsigned int vals[NUM_ELEMS]) {
+	clock_t start = clock();
+
+	long long int sum = 0;
+	for(unsigned int w = 0; w < OUTER_ITERATIONS; w++) {
+		for(unsigned int i = 0; i < NUM_ELEMS; i++) {
+			if(vals[i] >= 128) {
+				sum += vals[i];
+			}
+		}
+	}
+	clock_t end = clock();
+	printf("Time taken: %Lf s\n", (long double)(end - start) / CLOCKS_PER_SEC);
+	return sum;
+}
+
+long long int sum_unrolled(unsigned int vals[NUM_ELEMS]) {
+	clock_t start = clock();
+	long long int sum = 0;
+
+	for(unsigned int w = 0; w < OUTER_ITERATIONS; w++) {
+		for(unsigned int i = 0; i < NUM_ELEMS / 4 * 4; i += 4) {
+			if(vals[i] >= 128) sum += vals[i];
+			if(vals[i + 1] >= 128) sum += vals[i + 1];
+			if(vals[i + 2] >= 128) sum += vals[i + 2];
+			if(vals[i + 3] >= 128) sum += vals[i + 3];
+		}
+
+		//This is what we call the TAIL CASE
+		//For when NUM_ELEMS isn't a multiple of 4
+		//NONTRIVIAL FACT: NUM_ELEMS / 4 * 4 is the largest multiple of 4 less than NUM_ELEMS
+		for(unsigned int i = NUM_ELEMS / 4 * 4; i < NUM_ELEMS; i++) {
+			if (vals[i] >= 128) {
+				sum += vals[i];
+			}
+		}
+	}
+	clock_t end = clock();
+	printf("Time taken: %Lf s\n", (long double)(end - start) / CLOCKS_PER_SEC);
+	return sum;
+}
+
+long long int sum_simd(unsigned int vals[NUM_ELEMS]) {
+	clock_t start = clock();
+	__m128i _127 = _mm_set1_epi32(127);		// This is a vector with 127s in it... Why might you need this?
+	long long int result = 0;				   // This is where you should put your final result!
+	/* DO NOT DO NOT DO NOT DO NOT WRITE ANYTHING ABOVE THIS LINE. */
+	
+	for(unsigned int w = 0; w < OUTER_ITERATIONS; w++) {
+		/* YOUR CODE GOES HERE */
+        __m128i result_itr = _mm_setzero_si128();
+        for (unsigned int i = 0; i < NUM_ELEMS / 4 * 4; i += 4) {
+            __m128i vals_m128i = _mm_loadu_si128((__m128i *) &vals[i]);
+            result_itr = _mm_add_epi32(result_itr ,_mm_and_si128(vals_m128i, _mm_cmpgt_epi32(vals_m128i, _127)));
+        }
+        unsigned int results[4] = {0,0,0,0};
+        _mm_storeu_si128((__m128i *)results, result_itr);
+        for (int i = 0; i < 4; i ++) {
+            result += results[i];
+        }
+		/* You'll need a tail case. */
+        for (int i = NUM_ELEMS / 4 * 4; i < NUM_ELEMS; i ++) {
+            if(vals[i] >= 128) result += vals[i];
+        }
+	}
+	clock_t end = clock();
+	printf("Time taken: %Lf s\n", (long double)(end - start) / CLOCKS_PER_SEC);
+	return result;
+}
+
+long long int sum_simd_unrolled(unsigned int vals[NUM_ELEMS]) {
+	clock_t start = clock();
+	__m128i _127 = _mm_set1_epi32(127);
+	long long int result = 0;
+	for(unsigned int w = 0; w < OUTER_ITERATIONS; w++) {
+		/* COPY AND PASTE YOUR sum_simd() HERE */
+		/* MODIFY IT BY UNROLLING IT */
+        __m128i result_itr = _mm_setzero_si128();
+        for (unsigned int i = 0; i < NUM_ELEMS / 16 * 16; i += 16) {
+            __m128i vals_m128i = _mm_loadu_si128((__m128i *) &vals[i]);
+            result_itr = _mm_add_epi32(result_itr ,_mm_and_si128(vals_m128i, _mm_cmpgt_epi32(vals_m128i, _127)));
+            vals_m128i = _mm_loadu_si128((__m128i *) &vals[i + 4]);
+            result_itr = _mm_add_epi32(result_itr ,_mm_and_si128(vals_m128i, _mm_cmpgt_epi32(vals_m128i, _127)));
+            vals_m128i = _mm_loadu_si128((__m128i *) &vals[i + 8]);
+            result_itr = _mm_add_epi32(result_itr ,_mm_and_si128(vals_m128i, _mm_cmpgt_epi32(vals_m128i, _127)));
+            vals_m128i = _mm_loadu_si128((__m128i *) &vals[i + 12]);
+            result_itr = _mm_add_epi32(result_itr ,_mm_and_si128(vals_m128i, _mm_cmpgt_epi32(vals_m128i, _127)));
+        }
+        unsigned int results[4] = {0,0,0,0};
+        _mm_storeu_si128((__m128i *)results, result_itr);
+        for (int i = 0; i < 4; i ++) {
+            result += results[i];
+        }
+		/* You'll need 1 or maybe 2 tail cases here. */
+        for (int i = NUM_ELEMS / 16 * 16; i < NUM_ELEMS; i ++) {
+            if(vals[i] >= 128) result += vals[i];
+        }
+	}
+	clock_t end = clock();
+	printf("Time taken: %Lf s\n", (long double)(end - start) / CLOCKS_PER_SEC);
+	return result;
+}
--- lab01/run.sh
+++
@@ -1,5 +0,0 @@
-gcc -c ll_cycle.c
-gcc -c test_ll_cycle.c
-gcc -o test_ll_cycle test_ll_cycle.o ll_cycle.o
-./test_ll_cycle
-echo $?
