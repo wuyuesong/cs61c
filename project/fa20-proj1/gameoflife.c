d30f62c380aa73462f86cc86eb9d65f862f18921
wuyuesong <1377030423@qq.com>
2023-10-22 20:09:11 +0800

    proj1 finished

--- project/fa20-proj1/gameoflife.c
+++ project/fa20-proj1/gameoflife.c
@@ -23,6 +23,52 @@
 Color *evaluateOneCell(Image *image, int row, int col, uint32_t rule)
 {
 	//YOUR CODE HERE
+    // printf("%u\n", rule);
+    Color *color = (Color *)malloc(sizeof(Color));
+    char count[24];
+    memset(count, 0, sizeof(count));
+    memset(color, 0, sizeof(color));
+    int act_x[8] = {1, 1, 1, 0, 0, -1, -1, -1};
+    int act_y[8] = {-1, 0, 1, -1, 1, -1, 0, 1};
+    for (int i = 0; i < 8; i ++) {
+        int x = row + act_x[i];
+        int y = col + act_y[i];
+        x = (x + image->rows) % image->rows;
+        y = (y + image->cols) % image->cols;
+        for (int j = 0; j < 24; j ++) {
+            if (j < 8) {
+                if (((image->image[x][y].R) & (1 << j)) != 0) {
+                    count[j] ++;
+                }
+            } else if (j < 16) {
+                if (((image->image[x][y].G) & (1 << (j-8))) != 0) {
+                    count[j] ++;
+                }
+            } else {
+                if (((image->image[x][y].B) & (1 << (j-16))) != 0) {
+                    count[j] ++;
+                }
+            }
+        }
+    }
+    for (int j = 0; j < 24; j ++) {
+        int bais = 0;
+        if (j < 8) {
+            if ( image->image[row][col].R & (1 << j) != 0) bais = 9;
+            if (((1 << (count[j] + bais)) & rule) != 0)
+                color->R += (1 << j);
+        } else if (j < 16) {
+            if ( image->image[row][col].G & (1 << (j - 8)) != 0) bais = 9;
+            if (((1 << (count[j] + bais)) & rule) != 0)
+                color->G += (1 << (j-8));
+        } else {
+            if ( image->image[row][col].B & (1 << (j - 16)) != 0) bais = 9;
+            if (((1 << (count[j] + bais)) & rule) != 0)
+                color->B += (1 << (j-16));
+        }
+        
+    }
+    return color;
 }
 
 //The main body of Life; given an image and a rule, computes one iteration of the Game of Life.
@@ -30,6 +76,21 @@ Color *evaluateOneCell(Image *image, int row, int col, uint32_t rule)
 Image *life(Image *image, uint32_t rule)
 {
 	//YOUR CODE HERE
+    Image *ret = (Image *)malloc(sizeof(Image)); 
+    ret->rows = image->rows;
+    ret->cols = image->cols;
+    ret->image = (Color **)malloc((image->rows) * sizeof(Color *));
+    for (int i = 0; i < image->rows; i ++) {
+        ret->image[i] = (Color *)malloc((image->cols) * sizeof(Color));
+    }
+    for (int i = 0; i < image->rows; i ++) {
+        for (int j = 0; j < image->cols; j ++) {
+            Color *new_color = evaluateOneCell(image, i, j, rule);
+            ret->image[i][j] = *new_color;
+            free(new_color);
+        }
+    }
+    return ret;
 }
 
 /*
@@ -50,4 +111,23 @@ You may find it useful to copy the code from steganography.c, to start.
 int main(int argc, char **argv)
 {
 	//YOUR CODE HERE
+    char *filename;
+    uint32_t rule;
+    if (argc != 3) {
+        exit(-1);
+    }
+    filename = argv[1];
+    Image *image;
+    Image *next_image;
+    image = readData(filename);
+    if (image == NULL) {
+        exit(-1); 
+    }
+    rule = strtoul(argv[2], NULL, 16);
+    next_image = life(image, rule);
+    writeData(next_image);
+
+    freeImage(image);
+    freeImage(next_image);
+    return 0;
 }
